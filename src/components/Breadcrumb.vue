e03308ab1f3c602fc1ab93b2fc04ea8ca69de098
QuintionTang <QuintionTang@gmail.com>
2021-04-12 22:56:24 +0800

    feat:增加面包屑

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h5 class="font-weight-bold py-3 mb-0">默认</h5>
+        <div class="text-muted small mt-0 mb-4 d-block breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="/"><i class="feather icon-home"></i></a>
+                </li>
+                <li class="breadcrumb-item"><a href="#">仪表盘</a></li>
+                <li class="breadcrumb-item active">默认</li>
+            </ol>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "Breadcrumb",
+});
+</script>
--- src/views/dashboard/default/index.vue
+++ src/views/dashboard/default/index.vue
@@ -1,3 +1,29 @@
 <template>
-    <div>Content</div>
+    <div class="rwc-layout-content">
+        <div class="container-fluid flex-grow-1 container-p-y">
+            <Breadcrumb />
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="card mb-4">
+                        <div class="card-header with-elements">
+                            <h6 class="card-header-title mb-0">统计</h6>
+                            <div class="card-header-elements ml-auto"></div>
+                        </div>
+                        <div class="card-body">统计曲线</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import { defineComponent } from "vue";
+import Breadcrumb from "../../../components/Breadcrumb.vue";
+export default defineComponent({
+    name: "DefaultDashboard",
+    components: {
+        Breadcrumb,
+    },
+});
+</script>
